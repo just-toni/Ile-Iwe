593f060d7b52662e72e5fec4f1c76c090f78b645
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-11-01 13:35:30 +0100

    ILE_IWE

--- src/main/java/com/ileiwe/data/security/config/SecurityConfig.java
+++ src/main/java/com/ileiwe/data/security/config/SecurityConfig.java
@@ -1,10 +1,64 @@
 package com.ileiwe.data.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final LearningPartyServiceImpl userDetails;
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(LearningPartyServiceImpl userDetails, PasswordEncoder passwordEncoder) {
+        this.userDetails = userDetails;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public SecurityConfig(boolean disableDefaults, LearningPartyServiceImpl userDetails, PasswordEncoder passwordEncoder) {
+        super(disableDefaults);
+        this.userDetails = userDetails;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/api/instructor")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilter( new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(userDetails)
+                .passwordEncoder(passwordEncoder);
+    }
 }
